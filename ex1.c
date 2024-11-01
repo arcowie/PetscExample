0e250651efb7b746247abe703017fa203c6a98c5
Albert Cowie <arcowie@buffalo.edu>
2024-11-01 15:15:33 -0400

    Add ex1 to .gitignore and implement SetupProblem function in ex1.c
    - Added "ex1" to .gitignore
    - Implemented SetupProblem function in ex1.c for setting up problem parameters.

--- ex1.c
+++ ex1.c
@@ -11,9 +11,28 @@ static PetscErrorCode CreateMesh(MPI_Comm comm, DM *dm)
     PetscCall(DMViewFromOptions(*dm, NULL, "-dm_view"));
     PetscFunctionReturn(PETSC_SUCCESS);
     
-    
-    
 }
+
+static PetscErrorCode SetupProblem(DM dm)
+{
+    PetscFE        fe;
+    PetscInt       cStart, dim;
+    DMPolytopeType ct;
+    PetscDS        ds;
+
+    PetscFunctionBeginUser;
+    PetscCall(DMPlexGetHeightStratum(dm, 0, &cStart, NULL));
+    PetscCall(DMGetDimension(dm, &dim));
+    PetscCall(DMPlexGetCellType(dm, cStart, &ct));
+    PetscCall(PetscFECreateByCell(PETSC_COMM_SELF, dim, 1, ct, NULL, PETSC_DECIDE, &fe));
+    PetscCall(DMSetField(dm, 0, NULL, (PetscObject)fe));
+    PetscCall(PetscFEDestroy(&fe));
+    PetscCall(DMCreateDS(dm));
+    PetscCall(DMGetDS(dm, &ds));
+    
+    PetscFunctionReturn(PETSC_SUCCESS);
+}
+
 
 int main(int argc, char **argv)
 {
@@ -23,9 +42,15 @@ int main(int argc, char **argv)
 
     PetscCall(PetscInitialize(&argc, &argv, NULL, NULL));
     PetscCall(CreateMesh(PETSC_COMM_WORLD, &dm));
+    PetscCall(SetupProblem(dm));
+
+    PetscCall(DMCreateGlobalVector(dm, &u));
+    
+    
     
 
-    
+
+    PetscCall(VecDestroy(&u));
     
     PetscCall(DMDestroy(&dm));
     
